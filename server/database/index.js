7a3209d2ce162c188d81382270a955e7eb9dee3f
Sylvan Swierkosz <sswierkosz@gmail.com>
2022-03-01 14:49:01 -0800

    changes name in package.json | removes ssl options from database/index

--- server/database/index.js
+++ server/database/index.js
@@ -4,12 +4,6 @@ const pgp = require('pg-promise')
 
 const options = { connectionString: process.env.DATABASE_URL }
 
-if (process.env.NODE_ENV === 'production') {
-  options.ssl = {
-    rejectUnauthorized: false,
-  }
-}
-
 const db = pgp()
 const client = db(options)
 module.exports = client
