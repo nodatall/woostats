e5201eacc97550339d8df1000d34366f35676ba8
Sylvan Swierkosz <sswierkosz@gmail.com>
2022-03-02 07:12:21 -0800

    adds disclaimer to footer

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -5,6 +5,7 @@ import Box from "@mui/material/Box"
 import Stack from "@mui/material/Stack"
 import GitHubIcon from '@mui/icons-material/GitHub'
 import TwitterIcon from '@mui/icons-material/Twitter'
+import Typography from '@mui/material/Typography'
 
 import TopNav from "./TopNav"
 
@@ -33,25 +34,33 @@ export default function Layout({ children }) {
 
 
 function Footer() {
-  return <Stack sx={{ pb: 2, flexDirection: 'row', justifyContent: 'center'}}>
-    <a {...{
-      href: 'https://github.com/nodatall/woostats',
-      target: '_blank'
-    }}>
-      <GitHubIcon
-        fontSize="medium"
-        color="disabled"
-        sx={{ mr: 2 }}
-      />
-    </a>
-    <a {...{
-      href: 'https://twitter.com/7heThreeWords',
-      target: '_blank'
-    }}>
-      <TwitterIcon
-        fontSize="medium"
-        color="disabled"
-      />
-    </a>
+  return <Stack>
+    <Stack sx={{ pb: 1, flexDirection: 'row', justifyContent: 'center'}}>
+      <a {...{
+        href: 'https://github.com/nodatall/woostats',
+        target: '_blank'
+      }}>
+        <GitHubIcon
+          fontSize="medium"
+          color="disabled"
+          sx={{ mr: 2 }}
+        />
+      </a>
+      <a {...{
+        href: 'https://twitter.com/7heThreeWords',
+        target: '_blank'
+      }}>
+        <TwitterIcon
+          fontSize="medium"
+          color="disabled"
+        />
+      </a>
+    </Stack>
+    <Typography
+      variant="body1"
+      sx={{ textAlign: 'center', color: 'rgba(255, 255, 255, 0.3)', pb: 2 }}
+    >
+      Unofficial community site for the WOO Network & DAO
+    </ Typography>
   </Stack>
 }
