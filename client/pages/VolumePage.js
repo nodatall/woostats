c629683eb1b2e5a5364ffaa6029761343d1b0156
Sylvan Swierkosz <sswierkosz@gmail.com>
2022-03-03 13:31:36 -0800

    changes titles of total volume % graphs

--- client/pages/VolumePage.js
+++ client/pages/VolumePage.js
@@ -46,8 +46,8 @@ export default function VolumePage() {
   )
 
   const charts = [
-    { title: 'WOO % of total market volume', data: percentSeries },
-    { title: 'WOO % of total 50 day MA', data: percentSeries },
+    { title: 'WOO Network % of total market volume', data: percentSeries },
+    { title: 'WOO Network % of total 50 day MA', data: percentSeries },
     { title: 'Daily WOO Network volume', data: wooVolumeSeries },
     { title: 'Daily WOO volume 50 day MA', data: wooVolumeSeries },
     { title: 'Total crypto market volume', data: aggregateVolumeSeries },
