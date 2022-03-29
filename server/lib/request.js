ecee3d4f941be016899216d160ba253a609d495a
Sylvan Swierkosz <sswierkosz@gmail.com>
2022-03-29 16:33:07 -0700

    adds options and keyString is no longer required in request

--- server/lib/request.js
+++ server/lib/request.js
@@ -1,14 +1,24 @@
 const axios = require('axios')
 const logger = require('./logger')
 
-async function request({ name, method = 'get', serverUrl, keyString, path = '', params }) {
+async function request({
+  name,
+  method = 'get',
+  serverUrl,
+  keyString = '',
+  path = '',
+  params,
+  headers = {},
+}) {
   let result
 
   try {
-    let requestUrl = `${serverUrl}${path}?${keyString}`
+    let requestUrl = `${serverUrl}${path}`
+    if (keyString || params) requestUrl += `?${keyString}`
     logger.debug(`${name} ${requestUrl} ${params}`)
     if (params) requestUrl += params
-    const response = await axios[method](requestUrl)
+    const options = { ...headers }
+    const response = await axios[method](requestUrl, options)
     result = response.data
   } catch(error) {
     logger.log('error', `${name} ${error.config.url} ${JSON.stringify(error.response ? error.response.data : error)}`)
