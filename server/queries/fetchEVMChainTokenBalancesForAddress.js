6af82befe2c6e2bcf0ade421795d59f0f3d5dc9c
Sylvan Swierkosz <sswierkosz@gmail.com>
2022-09-09 21:28:34 -0700

    uses authentiacted debank when getting token balances

--- server/queries/fetchEVMChainTokenBalancesForAddress.js
+++ server/queries/fetchEVMChainTokenBalancesForAddress.js
@@ -3,6 +3,7 @@ const debankRequest = require('../lib/debank')
 module.exports = async function fetchEVMChainTokenBalancesForAddress({ address, chainId }) {
   return await debankRequest(
     `/v1/user/token_list`,
-    `id=${address}&chain_id=${chainId}&has_balance=true&is_all=false`
+    `id=${address}&chain_id=${chainId}&has_balance=true&is_all=false`,
+    true
   )
 }
