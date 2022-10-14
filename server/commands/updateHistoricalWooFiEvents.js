3099f6eadecf1ff012a0d4c6fe5cc65f515a0d70
Sylvan Swierkosz <sswierkosz@gmail.com>
2022-10-14 13:45:58 -0700

    wip

--- server/commands/updateHistoricalWooFiEvents.js
+++ server/commands/updateHistoricalWooFiEvents.js
@@ -6,6 +6,7 @@ module.exports = async function updateHistoricalWooFiEvents({ stream, from = 0 }
   let to = Infinity
   while (to > from) {
     const events = await fetchHistoricalEventsForNakjiStream({ stream, to: to === Infinity ? undefined : to })
+    if (events.length === 0) return
     await createWooFiEvents({ events })
     to = dayjs(events[events.length - 1].Data.ts).unix()
     if (events.length < 1000) break
