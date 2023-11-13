9435041dfe0f42019fb82155f07d99e6b248ca49
Sylvan Swierkosz <sswierkosz@gmail.com>
2023-11-13 12:29:22 -0800

    pushes csv to dune in updateWoofiProDailyVolumeHistory

--- server/commands/updateWoofiProDailyVolumeHistory.js
+++ server/commands/updateWoofiProDailyVolumeHistory.js
@@ -1,6 +1,8 @@
 const knex = require('../database/knex')
 const { client } = require('../database')
 const fetchWoofiProDailyVolumeHistory = require('../queries/fetchWoofiProDailyVolumeHistory')
+const request = require('../lib/request')
+const dayjs = require('../lib/dayjs')
 
 module.exports = async function updateWoofiProDailyVolumeHistory() {
   const volumeHistory = await fetchWoofiProDailyVolumeHistory()
@@ -19,4 +21,26 @@ module.exports = async function updateWoofiProDailyVolumeHistory() {
     [knex('woofi_pro_daily_volume_by_account').insert(update)],
   )
   await client.query(`${query}`)
+
+  const header = ['date', 'account_id', 'volume']
+
+  const csvRows = update.map(item => [
+    dayjs.utc(item.date).toISOString(),
+    item.account_id,
+    item.volume,
+  ].join(','))
+
+  const csv = [header.join(','), ...csvRows].join('\n')
+  await request({
+    name: `updateWoofiProDailyVolumeHistoryDuneTable`,
+    method: 'post',
+    serverUrl: 'https://api.dune.com/api/v1/table/upload/csv',
+    headers: {
+      'X-Dune-Api-Key': process.env.DUNE_API_KEY,
+    },
+    data: {
+      'table_name': 'woofi_pro_daily_volume_by_account',
+      data: csv,
+    }
+  })
 }
