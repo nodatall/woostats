57a655420dd64cd5cc9aff538234e800fcdfa9c3
Sylvan Swierkosz <sswierkosz@gmail.com>
2025-05-27 11:27:50 -0700

    uses etherscan api v2

--- server/lib/arbiscan.js
+++ server/lib/arbiscan.js
@@ -1,14 +1,20 @@
 const request = require('./request')
 
-const serverUrl = 'https://api.arbiscan.io/api'
-const keyString = `apikey=${process.env.ARBISCAN_API_KEY}`
+const serverUrl = 'https://api.etherscan.io/v2/api'
+const keyString = `apikey=${process.env.ETHERSCAN_API_KEY}`
 
-module.exports = async function arbicanRequest(params) {
-  const paramsWithKey = params ? `${keyString}&${params}` : keyString
+module.exports = async function arbiscanRequest(params) {
+  const paramsWithChain = params ? `chainid=42161&${params}` : 'chainid=42161'
+  const paramsWithKey = `${paramsWithChain}&${keyString}`
+
+  console.log('Request URL:', `${serverUrl}?${paramsWithKey}`)
 
   return await request({
     name: 'arbiscanRequest',
     serverUrl,
     params: paramsWithKey,
+    headers: {
+      'Accept': 'application/json'
+    }
   })
 }
