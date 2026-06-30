9dc307bfad2bcbfa8f735bdb13a4aec1ce6fc0b8
Sylvan Swierkosz <sswierkosz@gmail.com>
2026-06-30 13:02:01 -0700

    Remove WOO X from stats page

--- client/pages/NetworkPage/AggregateNetworkVolumeBox.js
+++ client/pages/NetworkPage/AggregateNetworkVolumeBox.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import numeral from 'numeral'
 
-import Divider from '@mui/material/Divider'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
 
@@ -11,23 +10,21 @@ import ContentCard from 'components/ContentCard'
 
 export default function AggregateNetworkVolumeBox() {
   const {
-    wooSpotVolumeToday,
-    wooFuturesVolumeToday,
-    woofiVolumeToday,
+    woofiVolumeToday = 0,
     woofiPro24hrVolume = 0,
   } = useAppState(
-    ['wooSpotVolumeToday', 'wooFuturesVolumeToday', 'woofiVolumeToday', 'woofiPro24hrVolume']
+    ['woofiVolumeToday', 'woofiPro24hrVolume']
   )
   const theme = useTheme()
   const stackBaseStyle = { flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }
 
-  const createVolumeElements = (spotVolume, futuresVolume, labelPrefix) => {
-    const categories = labelPrefix === 'Woofi' ? ['Pro', 'Swap'] : ['Futures', 'Spot']
+  const createVolumeElements = (swapVolume, proVolume) => {
+    const categories = ['Pro', 'Swap']
 
     return categories.map(category => {
-      const volume = category === 'Spot' || category === 'Swap' ? spotVolume : futuresVolume
+      const volume = category === 'Swap' ? swapVolume : proVolume
       return (
-        <Stack sx={{ ...stackBaseStyle, mr: category === 'Futures' || category === 'Pro' ? 2 : 0 }} key={category}>
+        <Stack sx={{ ...stackBaseStyle, mr: category === 'Pro' ? 2 : 0 }} key={category}>
           <Typography variant="h6" sx={{ textAlign: 'right', opacity: .6 }}>
             {`${category}`}
           </Typography>
@@ -39,34 +36,10 @@ export default function AggregateNetworkVolumeBox() {
     })
   }
 
-  const wooVolumesElements = createVolumeElements(wooSpotVolumeToday, wooFuturesVolumeToday, 'WOO X')
-  const woofiVolumesElements = createVolumeElements(woofiVolumeToday, woofiPro24hrVolume, 'Woofi')
+  const woofiVolumesElements = createVolumeElements(woofiVolumeToday, woofiPro24hrVolume)
 
   return <ContentCard>
     <Stack sx={{ alignItems: 'center', py: 2 }}>
-      {/* WOO X Volumes */}
-      <Stack sx={{
-        ...stackBaseStyle,
-        mb: 2,
-        [theme.breakpoints.down('sm')]: {
-          flexFlow: 'column-reverse',
-          '& > *:first-of-type': { mb: 1 },
-          mb: 0,
-        },
-      }}>
-        <Typography variant="h4" sx={{ color: 'secondary.main', mr: 2 }}>
-          ${numeral(+wooSpotVolumeToday + +wooFuturesVolumeToday).format('0,0')}
-        </Typography>
-        <Typography variant="h5">
-          24hr {<WooxLogo />} volume
-        </Typography>
-      </Stack>
-      <Stack sx={{ ...stackBaseStyle, flexWrap: 'wrap' }}>
-        {wooVolumesElements}
-      </Stack>
-      <Divider textAlign="center" sx={{ width: '260px', my: 2 }} />
-
-      {/* Woofi Volumes */}
       <Stack sx={{
         ...stackBaseStyle,
         mb: 2,
@@ -88,18 +61,6 @@ export default function AggregateNetworkVolumeBox() {
       </Stack>
     </Stack>
   </ContentCard>
-}
-
-function WooxLogo() {
-  return <svg height="15px" width="71px" focusable="false" viewBox="0 0 114 24">
-    <path fill="#c8c8c8" d="M47.9849 0C45.6482 0 43.3639 0.692837 41.4209 1.99108C39.4779 3.28933 37.9636 5.13463 37.0693 7.29353C36.1751 9.45243 35.9411 11.828 36.397 14.1199C36.8529 16.4118 37.9781 18.5172 39.6305 20.1695C41.2829 21.8219 43.3881 22.9471 45.68 23.403C47.9718 23.8589 50.3474 23.6249 52.5063 22.7306C54.6652 21.8364 56.5105 20.322 57.8087 18.379C59.107 16.436 59.7999 14.1518 59.7999 11.815C59.7999 8.68149 58.5551 5.67624 56.3394 3.4605C54.1236 1.24477 51.1185 0 47.9849 0ZM47.9849 18.176C46.7269 18.176 45.497 17.803 44.451 17.1041C43.4049 16.4051 42.5896 15.4115 42.1082 14.2492C41.6268 13.0869 41.5008 11.8081 41.7462 10.5742C41.9917 9.34026 42.5975 8.20686 43.4871 7.31727C44.3767 6.42768 45.5101 5.82181 46.744 5.57637C47.9779 5.33093 49.2569 5.4568 50.4192 5.93824C51.5815 6.41969 52.5749 7.23511 53.2739 8.28116C53.9728 9.32721 54.3459 10.5569 54.3459 11.815C54.3459 13.502 53.6757 15.1198 52.4828 16.3127C51.2899 17.5057 49.672 18.176 47.9849 18.176Z"></path>
-    <path fill="#c8c8c8" d="M74.6149 0C72.2782 0 69.9939 0.692837 68.0509 1.99108C66.1079 3.28933 64.5936 5.13463 63.6994 7.29353C62.8051 9.45243 62.5711 11.828 63.027 14.1199C63.4829 16.4118 64.6082 18.5172 66.2605 20.1695C67.9129 21.8219 70.0181 22.9471 72.31 23.403C74.6018 23.8589 76.9774 23.6249 79.1363 22.7306C81.2952 21.8364 83.1405 20.322 84.4387 18.379C85.737 16.436 86.4299 14.1518 86.4299 11.815C86.4299 8.68149 85.1851 5.67624 82.9694 3.4605C80.7537 1.24477 77.7485 0 74.6149 0ZM74.6149 18.176C73.3569 18.176 72.1271 17.803 71.081 17.1041C70.0349 16.4051 69.2196 15.4115 68.7382 14.2492C68.2568 13.0869 68.1308 11.8081 68.3762 10.5742C68.6217 9.34026 69.2275 8.20686 70.1171 7.31727C71.0067 6.42768 72.1401 5.82181 73.374 5.57637C74.6079 5.33093 75.8869 5.4568 77.0492 5.93824C78.2115 6.41969 79.2049 7.23511 79.9039 8.28116C80.6028 9.32721 80.9759 10.5569 80.9759 11.815C80.9759 13.502 80.3057 15.1198 79.1128 16.3127C77.9199 17.5057 76.302 18.176 74.6149 18.176Z"></path>
-    <path fill="#c8c8c8" d="M35.1899 0.195374H29.435L26.781 8.85571H32.4521L35.1899 0.195374Z"></path>
-    <path fill="#c8c8c8" d="M22.0318 8.43666L24.8813 15.1135L25.915 11.7052H31.5582L28.3734 22.0697C28.122 22.8799 27.3677 23.4386 26.5016 23.4386H23.2889C22.5067 23.4386 21.7804 22.9637 21.4731 22.2094L17.9531 13.6049L14.461 22.1815C14.1816 22.9078 13.4553 23.4107 12.6451 23.4107H9.40445C8.53842 23.4107 7.78413 22.8519 7.5327 22.0418L0.799988 0.195374H6.47111L10.941 15.1135L13.9023 8.38079C14.5448 6.70459 16.1651 5.61507 17.9531 5.61507C19.7689 5.61507 21.3893 6.73253 22.0318 8.43666Z"></path>
-    <path d="M98.9184 5.50455L94.8625 0.200012H87.8L95.3871 10.128L98.9184 5.50455Z" fill="#62CF7F"></path>
-    <path d="M113.126 0.200012H106.061L88.3031 23.44H95.3656L113.126 0.200012Z" fill="#62CF7F"></path>
-    <path d="M106.564 23.44L102.511 18.1355L106.042 13.512L113.629 23.44H106.564Z" fill="#62CF7F"></path>
-  </svg>
 }
 
 function WoofiLogo() {
@@ -126,4 +87,3 @@ function WoofiLogo() {
     <rect style={{ fill: 'url(#linear-gradient-3)'}} x="1584.57" y="137.3" width="74.89" height="196.58" />
   </svg>
 }
-
--- client/pages/NetworkPage/index.js
+++ client/pages/NetworkPage/index.js
@@ -1,18 +1,14 @@
-import React, { useRef, useState, useEffect, useMemo } from 'react'
+import React, { useMemo } from 'react'
 
 import { SMA } from 'technicalindicators'
 
 import { useAppState } from 'lib/appState'
 import { useLocalStorage } from 'lib/storageHooks'
 import { lineColors } from 'lib/chart'
-import { TOP_FUTURES_EXCHANGES } from 'lib/constants'
 
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
 
 import Loading from 'components/Loading'
 import TwoColumns from 'components/TwoColumns'
@@ -22,18 +18,10 @@ import AggregateNetworkVolumeBox from './AggregateNetworkVolumeBox'
 
 export default function NetworkPage() {
   const {
-    wooSpotVolume = [],
-    wooFuturesVolume,
-    topFuturesExchangeVolumes,
-    topSpotExchangeVolumes,
     woofiVolumeHistory = [],
     woofiProVolumeHistory = [],
   } = useAppState(
     [
-      'wooSpotVolume',
-      'wooFuturesVolume',
-      'topFuturesExchangeVolumes',
-      'topSpotExchangeVolumes',
       'woofiVolumeHistory',
       'woofiProVolumeHistory'
     ]
@@ -44,33 +32,8 @@ export default function NetworkPage() {
   }, [woofiProVolumeHistory, woofiVolumeHistory])
 
   if (
-    (!wooSpotVolume || wooSpotVolume.length === 0) ||
-    (!wooFuturesVolume || wooFuturesVolume.length === 0) ||
-    (!topFuturesExchangeVolumes || topFuturesExchangeVolumes.length == 0) ||
-    (!topSpotExchangeVolumes || topSpotExchangeVolumes.length == 0) ||
     (!woofiVolumeHistory || woofiVolumeHistory.length == 0)
   ) return <Loading />
-
-  const {
-    labels: wooVolumeLabels, series: wooVolumeSeries, wooSpotVolumeSeries, wooFuturesVolumeSeries,
-  } = wooSpotVolume
-    .reduce(
-      (acc, { date, volume }) => {
-        const { volume: futuresVolume } = wooFuturesVolume.find(futuresVol => futuresVol.date === date) || { volume: 0 }
-        acc.labels.push(date)
-        acc.series.push(+volume + +futuresVolume)
-        acc.wooSpotVolumeSeries.push(+volume)
-        acc.wooFuturesVolumeSeries.push(futuresVolume ? +futuresVolume : 0)
-        return acc
-      },
-      {
-        labels: [],
-        series: [],
-        wooSpotVolumeSeries: [],
-        wooFuturesVolumeSeries: [],
-      }
-    )
-
 
   const {
     woofiVolumeSeries,
@@ -101,18 +64,6 @@ export default function NetworkPage() {
     <AggregateNetworkVolumeBox />
     <TwoColumns>
       <VolumeOrLineChart {...{
-        title: 'Daily WOO X volume',
-        wooDailyChartData: { wooVolumeSeries, wooSpotVolumeSeries, wooFuturesVolumeSeries },
-        labels: wooVolumeLabels,
-      }}/>
-      <VolumeOrLineChart {...{
-        title: 'Daily WOO X volume [day] day MA',
-        datasets: [wooVolumeSeries],
-        labels: wooVolumeLabels,
-      }}/>
-    </TwoColumns>
-    <TwoColumns>
-      <VolumeOrLineChart {...{
         title: 'Daily WOOFi volume',
         wooDailyChartData: {
           wooVolumeSeries: woofiTotalVolumeSeries,
@@ -127,150 +78,7 @@ export default function NetworkPage() {
         labels: woofiVolumeLabels,
       }}/>
     </TwoColumns>
-    <FuturesComparisonCharts />
   </Box>
-}
-
-function FuturesComparisonCharts() {
-  const {
-    topFuturesExchangeVolumes, wooFuturesVolume, woofiProVolumeHistory
-  } = useAppState(
-    ['topFuturesExchangeVolumes', 'wooFuturesVolume', 'woofiProVolumeHistory']
-  )
-
-  const combinedWooFuturesVolume = useMemo(() => {
-    if (!wooFuturesVolume || !woofiProVolumeHistory) return wooFuturesVolume || []
-
-    const woofiProVolumeMap = woofiProVolumeHistory.reduce((map, entry) => {
-      map[entry.date] = +entry.volume
-      return map
-    }, {})
-
-    return wooFuturesVolume.map(entry => ({
-      ...entry,
-      volume: +entry.volume + (woofiProVolumeMap[entry.date] || 0)
-    }))
-  }, [wooFuturesVolume, woofiProVolumeHistory])
-
-  return <SelectAndMACharts {...{
-    wooVolumeSeries: combinedWooFuturesVolume,
-    localStorageKey: 'futuresComparisonDropdown',
-    storageDefault: 'aggregateFutures',
-    exchangeMap: TOP_FUTURES_EXCHANGES,
-    volumeHistories: topFuturesExchangeVolumes,
-  }}/>
-}
-
-
-function SelectAndMACharts({
-  localStorageKey, storageDefault, exchangeMap, wooVolumeSeries, volumeHistories,
-}) {
-  const componentRef = useRef()
-  const [savedHeight, setSavedHeight] = useState()
-  const [selectedExchange = storageDefault, selectExchange] = useLocalStorage(localStorageKey)
-
-  useEffect(() => {
-    if (componentRef.current) {
-      setSavedHeight(componentRef.current.offsetHeight);
-    }
-  }, [])
-
-  useEffect(() => {
-    if (componentRef.current && savedHeight) {
-      componentRef.current.style.height = `${savedHeight}px`
-    }
-  }, [savedHeight])
-
-  const handleExchangeChange = newExchange => {
-    setSavedHeight(componentRef.current.offsetHeight)
-    selectExchange(newExchange)
-  }
-
-  const title = exchangeMap[selectedExchange] || 'top exchanges'
-
-  const selectMenuItems = useMemo(() => {
-    const items = [<MenuItem value={storageDefault} key={storageDefault}>Top exchanges</MenuItem>]
-    return items.concat(
-      Object.keys(exchangeMap).map(topExchange => (
-        <MenuItem value={topExchange} key={topExchange}>
-          {exchangeMap[topExchange]}
-        </MenuItem>
-      ))
-    )
-  }, [exchangeMap, storageDefault])
-
-  const select = <FormControl
-    sx={{
-      width: 'fit-content',
-      mr: 2,
-      mt: 0,
-    }}
-  >
-    <Select
-      value={selectedExchange}
-      onChange={event => {
-        handleExchangeChange(event.target.value)
-      }}
-      sx={{
-        '.MuiSelect-select': { py: .75 },
-        '&:hover fieldset.MuiOutlinedInput-notchedOutline': {
-          borderColor: 'rgb(213, 213, 213, 0.5)',
-          borderWidth: '1px',
-        },
-        '&.Mui-focused fieldset.MuiOutlinedInput-notchedOutline': {
-          borderColor: 'rgb(213, 213, 213, 0.5)',
-          borderWidth: '1px',
-        }
-      }}
-    >
-      {selectMenuItems}
-    </Select>
-  </ FormControl>
-
-  const currentExchangeSeries = useMemo(() => {
-    return volumeHistories.find(([topExchange]) => topExchange === selectedExchange)[1];
-  }, [selectedExchange, volumeHistories])
-
-  const wooVolumesMap = {}
-  wooVolumeSeries.forEach(({ date, volume }) => wooVolumesMap[date] = volume)
-
-  const { percentSeries, labels } = currentExchangeSeries
-    .reduce(
-      (acc, { volume, date }, index) => {
-        if (!wooVolumesMap[date]) return acc
-        let percent = (wooVolumesMap[date] / volume) * 100
-        if (percent > 1600) {
-          percent = acc.percentSeries[index - 1]
-        }
-        acc.percentSeries.push(percent)
-        acc.labels.push(date)
-        return acc
-      },
-      {
-        percentSeries: [],
-        labels: []
-      }
-    )
-
-  const spotFut = localStorageKey.includes('spot') ? 'spot' : 'futures'
-
-  return <TwoColumns>
-    <div ref={componentRef}>
-      <VolumeOrLineChart {...{
-        title: `WOO % ${spotFut} marketshare vs [${title}]`,
-        selectValue: title,
-        datasets: [percentSeries],
-        labels,
-        select,
-      }} />
-    </div>
-    <VolumeOrLineChart {...{
-      title: `WOO % ${spotFut} vs ${title} [day] day MA`,
-      datasets: [percentSeries],
-      labels,
-    }} />
-
-  </TwoColumns>
 }
 
 function VolumeOrLineChart({ title, datasets, wooDailyChartData, labels, select, selectValue }) {
@@ -291,7 +99,7 @@ function VolumeOrLineChart({ title, datasets, wooDailyChartData, labels, select,
   if (title.includes('%')) props.denominator = '%'
   if (title.includes('MA')) chart = <MAChart {...props} />
   else {
-    chart = (title === 'Daily WOO X volume') || (title === 'Daily WOOFi volume')
+    chart = title === 'Daily WOOFi volume'
       ? <DailyVolumeChart {...{ title, ...wooDailyChartData, ...props }} />
       : <RangeSliderLineChart {...props} />
   }
@@ -316,9 +124,8 @@ function MAChart({ ...props }) {
 }
 
 function DailyVolumeChart({ wooVolumeSeries, wooSpotVolumeSeries, wooFuturesVolumeSeries, ...props }) {
-  const localStorageKey = props.title === 'Daily WOOFi volume' ? 'woofiDailyVolumeToggle' : 'wooXDailyVolumeToggle'
-  const [isTotal = 1, setIsTotal] = useLocalStorage(localStorageKey)
-  const chartKey = props.title.includes('WOO X') ? 'wooXVolumeChart' : 'woofiVolumeChart'
+  const [isTotal = 1, setIsTotal] = useLocalStorage('woofiDailyVolumeToggle')
+  const chartKey = 'woofiVolumeChart'
 
   if (wooSpotVolumeSeries.length && wooFuturesVolumeSeries.length) {
     const lastSpotVolume = wooSpotVolumeSeries[wooSpotVolumeSeries.length - 1]
@@ -334,7 +141,7 @@ function DailyVolumeChart({ wooVolumeSeries, wooSpotVolumeSeries, wooFuturesVolu
   if (isTotal) {
     props.datasets = [{ data: wooVolumeSeries }]
   } else {
-    props.title = props.title === 'Daily WOOFi volume' ? 'WOOFi swap vs pro' : `WOO X spot vs futures`
+    props.title = 'WOOFi swap vs pro'
     props.datasets = [
       { data: wooFuturesVolumeSeries },
       { data: wooSpotVolumeSeries.slice(-(wooFuturesVolumeSeries.length)) },
@@ -342,23 +149,14 @@ function DailyVolumeChart({ wooVolumeSeries, wooSpotVolumeSeries, wooFuturesVolu
     props.labels = props.labels.slice(-(wooFuturesVolumeSeries.length))
   }
 
-  const subtitleElements = props.title.includes('WOOFi') ?
-    [
-      <Stack direction="row">
-        <Typography sx={{ color: lineColors[1] }} component="span">Swap</Typography>
-        &nbsp;&nbsp;vs&nbsp;&nbsp;
-        <Typography sx={{ color: lineColors[0] }} component="span">Pro</Typography>
-      </Stack>,
-      'Total',
-    ] :
-    [
-      <Stack direction="row">
-        <Typography sx={{ color: lineColors[1] }} component="span">Spot</Typography>
-        &nbsp;&nbsp;vs&nbsp;&nbsp;
-        <Typography sx={{ color: lineColors[0] }} component="span">Futures</Typography>
-      </Stack>,
-      'Total',
-    ]
+  const subtitleElements = [
+    <Stack direction="row">
+      <Typography sx={{ color: lineColors[1] }} component="span">Swap</Typography>
+      &nbsp;&nbsp;vs&nbsp;&nbsp;
+      <Typography sx={{ color: lineColors[0] }} component="span">Pro</Typography>
+    </Stack>,
+    'Total',
+  ]
 
   props.subtitle = <ButtonGroupSelector {...{
     sx: { mt: 1 },
